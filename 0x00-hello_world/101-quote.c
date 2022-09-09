e4431c29b6d67ab295c59d251eb5c6fe3bfc8933
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-09 05:01:46 +0000

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
