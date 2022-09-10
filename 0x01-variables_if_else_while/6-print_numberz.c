48cd015ef4678f263a704e69a4badd21097e92a6
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-10 02:32:47 +0000

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - whateves
+ *
+ * Return: gooo
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig <= 9; dig++)
+		putchar(dig);
+	putchar('\n');
+
+	return (0);
+}
