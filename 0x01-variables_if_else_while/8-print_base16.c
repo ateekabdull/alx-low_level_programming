6ced90df29a8fd4b0cef06ffd06be9fcb575bde8
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-10 02:52:23 +0000

    d

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,11 @@
 int main(void)
 {
 	char hex;
+	char dec;
 
-	for (hex = '0'; hex <= 'f'; hex++)
+	for (hex = '0'; hex <= '9'; hex++)
 		putchar(hex);
+	for (dec = 'a'; dec <= 'f'; dec++)
 	putchar('\n');
 
 	return (0);
