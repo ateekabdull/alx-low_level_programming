c082e5b9e9a58dc63b79e4ed665a21aa2650474d
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-10 02:20:34 +0000

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - yeah
+ *
+ * Return: walan
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num >= 9; num++)
+		printf("%d", num);
+	printf("\n");
+
+	return (0);
+}
