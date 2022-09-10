54fa194e11a10fbff6904514f0e818f2444fae22
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-10 01:56:31 +0000

    d

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l_case = 'a';
+	char u_case = 'A';
+
+	for (l_case = 'a'; l_case <= 'z'; l_case++)
+		putchar(l_case);
+	for (u_case = 'A'; u_case <= 'Z'; u_case++)
+		putchar(u_case);
+	putchar('\n');
+
+	return (0);
+}
