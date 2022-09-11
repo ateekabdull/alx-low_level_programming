d9664c2f95b839227664981495018bafd3d12b17
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-11 05:06:47 +0000

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - ddffdbfd
+ *
+ * Return: jfjkjjjr
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = '0'; dig <= '9'; dig++)
+	{
+		putchar(dig);
+		putchar(",");
+		putchar(" ");
+	}
+	putchar('\n');
+	return (0);
+}
