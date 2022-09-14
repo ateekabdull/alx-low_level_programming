b3a2945140da0c0397517a75b1ba3cf35e1c0bf1
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-14 03:35:51 +0000

    d

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - dsnmnmds
+ *
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	for (minute = 0; minute <= 59; minute++)
+	{
+		_putchar('0' + hour / 10);
+		_putchar('0' + hour % 10);
+		_purchar(':');
+		_putchar('0' + minute / 10);
+		_putchar('0' + minute % 10);
+		_putchar('\n');
+	}
+
+}
