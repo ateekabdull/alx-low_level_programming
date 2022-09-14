45c1529ea03f7417bf4f6885f37eaa65e062b8b9
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-14 02:59:24 +0000

    d

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - jdfkkjfd
+ *
+ *@c: dslkdlds
+ *
+ * Return: dfm
+ */
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
