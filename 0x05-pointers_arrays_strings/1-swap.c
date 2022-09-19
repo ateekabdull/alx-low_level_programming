8284d2eddab0ec5d84930b26fe79db474dab1a0f
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-19 07:23:45 +0000

    d

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: nada
+ */
+void swap_int(int *a, int *b);
+{
+	*a = &b;
+	*b = &a;
+}
