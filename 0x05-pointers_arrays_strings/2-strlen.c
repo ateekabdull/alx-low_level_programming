6664b63e11cf02f0e49491a69ba081d88a31b8a2
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-19 07:54:32 +0000

    d

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int length = sizeof(*s) / sizeof(char);
+}
