9357f72d82d89c82c911f8c698706dc922d190bc
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-20 01:32:16 +0000

    f

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: is the string
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i;
+
+	i = _strlen(s) - 1;
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+
+}
+
+/**
+ * _strlen - kjfjd
+ *
+ * @p: ldflkdf
+ *
+ * Return: k
+ */
+void _strlen(char *p)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
