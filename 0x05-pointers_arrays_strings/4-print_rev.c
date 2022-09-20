5154078ec9d41ab058e173c617c967989a5d3218
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-20 00:53:26 +0000

    d

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: the string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i, length, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	length = i;
+	
+	for (n = length - 1; n >= 0; i--)
+		_putchar(s[n]);
+
+
+
+}
