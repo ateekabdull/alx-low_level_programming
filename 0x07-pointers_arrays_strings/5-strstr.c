f228ed6d335cf33fc672b2c5e26569570af10285
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-28 02:18:49 +0000

    d

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * *_strstr - jfjfkjd
+ * @haystack: fldf
+ * @needle: fdjkkdf
+ * Return: dfdfkj
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *a;
+	char *b;
+
+	b = needle;
+
+	if (*b == 0)
+		return (haystack);
+	for (; haystack != 0; haystack += 1)
+	{
+		if (*haystack != *b)
+			continue;
+		a = haystack;
+		while (1)
+		{
+			if (*b == 0)
+				return (haystack);
+			if (*a++ != *b++)
+				break;
+		}
+		b  = needle;
+	}
+	return (0);
+}
