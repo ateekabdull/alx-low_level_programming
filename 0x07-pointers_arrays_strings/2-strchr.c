c53232214da58d7146931e89ec74b3ee8ecd3ead
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-28 01:34:59 +0000

    d

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character string
+ *
+ * @s: the pointer
+ * @c: the character string
+ *
+ * Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
