5a88fa7b8081dc8473270a1c22a33e10cb1ad1d3
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-29 03:29:54 +0000

    s

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * wildcmp - dskjdsk
+ * @s1: slsds
+ * @s2: kjjkcx
+ * Return: dfkdf
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
+		return (1);
+	if (*s2 == '*' && *(s1 + 1) && *s2)
+		return (0);
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,3 +10,4 @@ int is_prime_number(int n);
 int is_prime(int, int);
 int is_palindrome(char *s);
 int pal(char *s, int i, int j);
+int wildcmp(char *s1, char *s2);
