679f19bef9e3836d2e371d46b4f5b1337943cd33
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-29 03:24:31 +0000

    s

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -29,3 +29,15 @@ int pal(char *s, int i, int j)
 		return (pal(s, i + 1, j - 1));
 	return (1);
 }
+
+/**
+ * _strlen_recursion - sjdkkdjs
+ * @s: kldl
+ * Return: ds
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
