64b276c2eece284187c7a7c1b1b8bbcecfa0f6fc
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-09-30 23:40:25 +0000

    d

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,9 @@
  * @argv: array
  * Return: zero
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 1)
+		printf("%s\n", argv[0]);
+	return (0);
 }
