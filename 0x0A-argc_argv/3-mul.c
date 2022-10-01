eebb4b96abbd116f3cad527257fb849f606b3a9a
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-10-01 00:16:39 +0000

    d

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,10 +14,11 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
 	else
 	{
-		printf("ERROR");
+		printf("Error\n");
 		return (1);
 	}
 
