fe1f69549f810a1898e8df154e2ad42bb41a27c2
ABUBAKAR ABDULKADIR ABDULMUIZ <111713812+ateekabdull@users.noreply.github.com>
2022-10-02 00:48:02 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - adds up
- * @argc: count
- * @argv: array
- * Return: zero
- */
+  * main - prints its name, followed by new.
+  * @argc: argument counter.
+  * @argv: argument vector.
+  * Return: zero.
+  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
+	int i, j, sum = 0;
 
 	if (argc == 1)
-		printf("0\n");
+	{
+		printf("%d\n", argc - 1);
+		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 			if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
 			{
 				printf("Error\n");
-				Return (1);
+				return (1);
 			}
 		sum += atoi(argv[i]);
 	}
