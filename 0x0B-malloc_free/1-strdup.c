e524e5d31871459b995335b79f066d3ff8eb1ec9
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-10-10 06:11:45 +0000

    s

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * _strlen - returns the lenght of a string
+ *
+ * @s: the string
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * _strdup - duplicates a string
+ *
+ * @str: the string
+ *
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *temp;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	temp = (char *)malloc(sizeof(char *_strlen(str) + 1));
+
+	for (i = 0; i < _strlen(str) - 1; i++)
+	{
+		temp[i] = str[i];
+	}
+	return (temp);
+
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,1 +1,3 @@
 int _putchar(char c);
+int _strlen(char *s);
+char *_strdup(char *str);
