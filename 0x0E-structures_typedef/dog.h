11ddd4e5562dc7910ee0df837d22ed8df4f7a4cd
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-10-10 20:28:54 +0000

    f

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - defines a new struct
  * @name: name
@@ -14,9 +13,8 @@ struct dog
 	float age;
 	char *owner;
 };
-
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+#endif
