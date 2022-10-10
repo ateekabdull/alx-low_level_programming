323d15392c0f3114255270b6948603903884176c
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-10-10 21:08:49 +0000

    f

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,12 +2,13 @@
 
 /**
  * init_dog - initilizes a struct
+ * struct dog - struct
  *
  * @d: pointer to struct
  * @name: name
  * @age: age
  * @owner: owner
- * 
+ *
  * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -15,4 +16,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	if (d == NULL)
+		return;
 }
