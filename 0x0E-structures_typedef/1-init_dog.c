34deca119505eb36544645a464dcee7ed500a653
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-10-10 20:56:08 +0000

    f

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	name = d->name; 
-	age = d->age;
-	owner = d->owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
