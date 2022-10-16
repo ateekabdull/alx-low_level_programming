c5ab8b1e3861885e49816f15716f0053c9c555b3
ABUBAKAR ABDULKADIR <ateekabdul@yahoo.com>
2022-10-16 05:58:36 +0000

    s

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: the string
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *new_str;
+	int i = 0, j = 0;
+	
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (new_str == NULL)
+		return (NULL);
+
+	new_str = malloc(sizeof(char) * _strlen(new_str) + 1);
+
+	while (s1[i] != '\0')
+	{
+		new_str[j] = s1[i];
+		i++;
+		j++;
+	}
+	while (s2[i] != '\0')
+	{
+		new_str[j] = s2[i];
+		i++;
+		j++;
+	}
+	new_str[j] = '\0';
+
+	return (new_str);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,5 @@
 int _putchar(char c);
 int _strlen(char *s);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+
